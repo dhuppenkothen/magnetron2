35111237fa36c4681aa537433e5dd0c6c5ee388e
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2016-02-12 11:44:16 +1300

    Added Data.cpp

---
+++ code/Data.cpp
@@ -0,0 +1,56 @@
+#include "Data.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+Data Data::instance;
+
+Data::Data()
+{
+
+}
+
+void Data::load(const char* filename)
+{
+	fstream fin(filename, ios::in);
+	if(!fin)
+	{
+		cerr<<"# Failed to open file "<<filename<<"."<<endl;
+		return;
+	}
+
+	t.clear();
+	y.clear();
+
+	double temp1, temp2;
+	while(fin>>temp1 && fin>>temp2)
+	{
+		t.push_back(temp1);
+		y.push_back(temp2);
+	}
+
+	fin.close();
+	cout<<"# Found "<<t.size()<<" points in file "<<filename<<"."<<endl;
+
+	compute_summaries();
+}
+
+void Data::compute_summaries()
+{
+	t_min = *min_element(t.begin(), t.end());
+	t_max = *max_element(t.begin(), t.end());
+	t_range = t_max - t_min;
+	dt = t[1] - t[0];
+
+	// Left and right edges of the data bins
+	t_left.assign(t.size(), 0.);
+	t_right.assign(t.size(), 0.);
+	for(size_t i=0; i<t.size(); i++)
+	{
+		t_left[i] = t[i] - 0.5*dt;
+		t_right[i] = t[i] + 0.5*dt;
+	}
+}
+
