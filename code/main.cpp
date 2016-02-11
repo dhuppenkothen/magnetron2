08fafc91c716958d95d6f4e2a7d19949933ca51f
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2016-02-12 11:47:39 +1300

    added options

--- code/main.cpp
+++ code/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "DNest4/code/Start.h"
 #include "MyModel.h"
+#include "Data.h"
 
+using namespace std;
 using namespace DNest4;
 
 int main(int argc, char** argv)
 {
-	DNest4::start<MyModel>(argc, argv);
+	Data::get_instance().load("sample_file.txt")
+	Sampler<MyModel> sampler = setup<MyModel>(argc, argv);
+	sampler.run();
 	return 0;
 }
 
