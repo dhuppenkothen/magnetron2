ee8a456eb18efaca64e2f52e4dac4b0a051b96db
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-02-12 13:29:26 +1300

    Bug debugged :)

--- code/MyModel.cpp
+++ code/MyModel.cpp
@@ -12,9 +12,10 @@ const Data& MyModel::data = Data::get_instance();
 MyModel::MyModel()
 :bursts(4, 100, false, MyConditionalPrior(data.get_t_min(), data.get_t_max(),
                 1E-10, 5.0*3.5e5*data.get_dt()))
+,noise_normals(data.get_t().size())
 ,mu(data.get_t().size())
 {
-cout << mu.size() << endl;
+
 }
 
 void MyModel::calculate_mu()
