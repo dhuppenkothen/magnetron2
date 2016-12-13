5fdbb56f465e1a7fddd2cb7801b0fa5a2f7659e7
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-12-13 20:31:12 +1300

    Can specify data file with -d, saves run filename to a text file

--- code/main.cpp
+++ code/main.cpp
@@ -8,9 +8,25 @@ using namespace DNest4;
 
 int main(int argc, char** argv)
 {
-	Data::get_instance().load("../data/sample_data.txt");
-	Sampler<MyModel> sampler = setup<MyModel>(argc, argv);
-	sampler.run();
+    // Process command line options
+    CommandLineOptions clo(argc, argv);
+
+    // Get specified data file. If none, use a default.
+    std::string data_file = clo.get_data_file();
+    if(data_file.length() == 0)
+        data_file = std::string("../data/sample_data.txt");
+
+    // Save the data filename
+    std::fstream fout("run_data.txt", std::ios::out);
+    fout<<data_file;
+    fout.close();
+
+    // Load data
+    Data::get_instance().load(data_file.c_str());
+
+    // Run DNest4.
+    start<MyModel>(clo);
+
 	return 0;
 }
 
